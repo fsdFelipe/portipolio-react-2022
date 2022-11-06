ac18262b0b84569feec96f45fbf51db320fdefeb
FelipeDias <devfsdfelipe@gmail.com>
2022-11-06 16:08:20 +0000

    Navbar criado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import "./styles.css";
 import Header from "../src/components/Header.jsx";
+import Navbar from "../src/components/Navbar.jsx";
+import React, { useState } from "react";
 
 export default function App() {
-  return <Header />;
+  const [page, setPage] = useState("Header");
+  return (
+    <>
+      <Navbar page={page} setPage={setPage} />
+      {page == "Header" && <Header />}
+      {page == "About US" && <About />}
+      {page == "Portfolio" && <Portfolio />}
+      {page == "Skills" && <Skills />}
+      {page == "Services" && <Services />}
+      {page == "Contact US" && <ContactUS />}
+    </>
+  );
 }
