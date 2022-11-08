bac9caea5cb396f612f176de35db91d2b523f307
fsdFelipe <98924712+fsdFelipe@users.noreply.github.com>
2022-11-08 10:46:00 -0300

    Css de Footer e pequenas  atualizações

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import AboutStats from "../Settings/SobreStats.jsx";
+import SobreStats from "../Settings/SobreStats.jsx";
 import Settings from "../Settings/Settings.jsx";
 import "../Styles/Sobre.css";
 
 export default function Sobre() {
   return (
     <div className="about container">
-      <h2>About Me</h2>
+      <h2>Sobre</h2>
       <div className="profile">
         <img
           src={Settings.profile_photo}
@@ -15,17 +15,17 @@ export default function Sobre() {
           height="90px"
         />
         <div className="info">
-          <p> Name : {Settings.name}</p>
-          <p> Age : {Settings.Age}</p>
-          <p> Birthday : {Settings.Birthday}</p>
-          <p> Country : {Settings.Country}</p>
+          <p> Nome : {Settings.name}</p>
+          <p> Idade : {Settings.Age}</p>
+          <p> Data de Nascimento : {Settings.Birthday}</p>
+          <p> Pais : {Settings.Country}</p>
           <p>
             Email : <a href={`mailto:${Settings.Email}`}>{Settings.Email}</a>
           </p>
         </div>
       </div>
       <div className="stats">
-        {AboutStats.map((item, index) => (
+        {SobreStats.map((item, index) => (
           <div className="card" key={index}>
             <i>{item.icon} </i>
             <h3>{item.title}</h3>
